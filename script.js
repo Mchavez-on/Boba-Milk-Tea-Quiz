41a96daec9517ce1a60fc6be6976d416b5c0d18f
Glitch (personality-quiz-starter-code) <none>
2020-08-06 05:12:56 +0000

    🖐🖼 Checkpoint ./script.js:19920582/411

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //Global Variables
 //This is where you will define the variables you will be using in your project.
 //#TODO: Create four variables to track each possible quiz outcome
-var questionCount = 0;
-var result1Score = 0;
+var blackMilkTea = 0;
+var thaiMilkTea = 0;
+var honeyDewMilkTea = 0;
+var taroMilkTea = 0;
 
 
 
@@ -10,6 +12,9 @@ var result1Score = 0;
 
 //#TODO: Use the DOM to create variables for the first quiz question.
 var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById("q1a2");
+var q1a3 = document.getElementById("q1a3");
+var q1a4 = document.getElementById("q1a4");
 
 
 
@@ -17,7 +22,13 @@ var q1a1 = document.getElementById("q1a1");
 
 
 //#TODO: Add Event Listeners to your answer choice variables.
-q1a1.addEventListener("click", result1);
+q1a1.addEventListener("click", );
+q1a2.addEventListener("click", );
+q1a3.addEventListener("click", );
+q1a4.addEventListener("click", result4);
+
+
+
 
 
 
